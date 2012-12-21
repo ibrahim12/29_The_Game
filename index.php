5f7a4110560bba89ecbc504b121360043b4080a1
Md ibrahim Rashid <ibrahim12@gmail.com>
2012-12-21 13:15:59 -0500

    sdf

---
+++ index.php
@@ -0,0 +1,13 @@
+
+<html>
+<head>
+    <script type='text/javascript' href='script.js' />
+</head>
+
+<body onload="start()">    
+  <canvas id="glcanvas" width="640" height="480">
+    Your browser doesn't appear to support the HTML5 <code>&lt;canvas&gt;</code> element.
+  </canvas>
+</body>
+
+</html>
